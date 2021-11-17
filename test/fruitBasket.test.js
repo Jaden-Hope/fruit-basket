4b46da193ff17d238b6ee19abd9c25f681650652
Jaden Hope <Jaden_Hope@Yahoo.com>
2021-11-17 12:27:10 +0200

    minor changes

--- test/fruitBasket.test.js
+++ test/fruitBasket.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const Factory = require('../fruitBasketFactory');
 
 const { Pool } = require('pg');
-    
+
 // const connectionString = process.env.DATABASE_URL || 'postgres://jaden:mypass@localhost:5432/fruitbasket';
 const connectionString = 'postgres://postgres@localhost/fruitbasket';
 
@@ -16,11 +16,11 @@ const pool = new Pool({
 const factory = Factory(pool);
 
 describe('Fruit Basket testing:', () => {
-    beforeEach(async() => {
+    beforeEach(async () => {
         await pool.query('TRUNCATE TABLE fruit_basket');
     })
 
-    it('Should insert new fruit basket', async() => {
+    it('Should insert new fruit basket', async () => {
         await factory.createBasket('Apple', 15, 3);
 
         const out = await (await pool.query("SELECT * FROM fruit_basket")).rows[0];
@@ -30,7 +30,7 @@ describe('Fruit Basket testing:', () => {
         assert.equal(out.unit_price, 3);
     })
 
-    it('Should return all apple fruit baskets', async() => {
+    it('Should return all apple fruit baskets', async () => {
         await factory.createBasket('Apple', 15, 3);
         await factory.createBasket('Pear', 20, 2);
         await factory.createBasket('Apple', 25, 2);
@@ -46,7 +46,7 @@ describe('Fruit Basket testing:', () => {
         assert.equal(out[1].unit_price, 2);
     })
 
-    it("Should update a specific baskets' quantity", async() => {
+    it("Should update a specific baskets' quantity", async () => {
         await factory.createBasket('Apple', 15, 3);
 
         await factory.updateBasket('Apple', 15, 25);
@@ -57,13 +57,13 @@ describe('Fruit Basket testing:', () => {
         assert.equal(out[0].unit_price, 3);
     })
 
-    it("Should total a specific baskets' price", async() => {
+    it("Should total a specific baskets' price", async () => {
         await factory.createBasket('Apple', 15, 3);
 
         assert.equal(await factory.totalBasket('Apple', 15, 3), 45);
     })
 
-    it('Should display the total of all baskets of a given fruit', async() => {
+    it('Should display the total of all baskets of a given fruit', async () => {
         await factory.createBasket('Apple', 15, 3);
         await factory.createBasket('Apple', 20, 2);
         await factory.createBasket('Apple', 20, 3);
